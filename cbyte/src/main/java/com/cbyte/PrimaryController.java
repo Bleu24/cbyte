7080756bf8dddb9ed2524d083b26c0a007f80c58
ceanne. <g2.jankosovich@gmail.com>
2024-10-11 20:32:24 +0800

    - improved GUI - added custom buttons functionalities - filechooser code ongoing
    to do:
    - embed file chooser in center pane
    - figure out how the bottom terminal works
    - improve GUI
    - entire back-end code

--- cbyte/src/main/java/com/cbyte/PrimaryController.java
+++ cbyte/src/main/java/com/cbyte/PrimaryController.java
@@ -1,12 +1,39 @@
 package com.cbyte;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    Button minBtn, maxBtn, closeBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FileChooser fileChooser = new FileChooser();
+
+    }
+
+    //button methods
+    @FXML
+    private void onClose() throws IOException {
+       App.onClose();
+    }
+
+    @FXML
+    private void onMax() throws IOException {
+        App.onMax();
+    }
+
+    @FXML
+    private void onMin() throws IOException {
+        App.onMin();
     }
 }
