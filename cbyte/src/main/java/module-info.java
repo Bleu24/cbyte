565b543e87e8bc40071dc551ca914de0c6b84664
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-10-13 14:29:41 +0800

    updates and new builds, nothing to worry about

--- cbyte/src/main/java/module-info.java
+++ cbyte/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.cbyte {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.web;
+    requires com.kodedu.terminalfx;
+    
     opens com.cbyte to javafx.fxml;
     exports com.cbyte;
 }
