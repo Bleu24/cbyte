067d75845f4d23d05a9c5f9fccfcc8696a70b755
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-10-13 14:04:40 +0800

    refactored code

--- cbyte/src/main/java/com/cbyte/App.java
+++ cbyte/src/main/java/com/cbyte/App.java
@@ -3,6 +3,7 @@ package com.cbyte;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,16 +17,32 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-    private static Stage stage;
+    private static Stage thisStage;
+    
     
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        stage = primaryStage;
+    public void start(Stage stage) throws IOException {
+        
+    
         scene = new Scene(loadFXML("primary"));
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setScene(scene);
-        stage.show();
+        scene.getStylesheets().add(App.class.getResource("/styles.css").toExternalForm());
+        
+        thisStage = stage;
+        thisStage.setTitle("TerminalFX");
+        thisStage.initStyle(StageStyle.UNDECORATED);
+        thisStage.setScene(scene);
+        thisStage.show();
     }
+
+        /**
+             * The main() method is ignored in correctly deployed JavaFX application.
+             * main() serves only as fallback in case the application can not be
+             * launched through deployment artifacts, e.g., in IDEs with limited FX
+             * support. NetBeans ignores main().
+             *
+             * @param args the command line arguments
+             */
+    
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
@@ -33,6 +50,7 @@ public class App extends Application {
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
         return fxmlLoader.load();
     }
 
@@ -42,11 +60,11 @@ public class App extends Application {
 
     //button methods
     public static void onMax() {
-        stage.setMaximized(!stage.isMaximized());
+        thisStage.setMaximized(!thisStage.isMaximized());
     }
 
     public static void onMin() {
-        stage.setIconified(true);
+        thisStage.setIconified(true);
     }
 
     public static void onClose() {
