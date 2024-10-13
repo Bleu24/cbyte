83ff662a033ee351da04857ae3f6ef63ac94f4b5
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-10-13 14:03:42 +0800

    added TerminalFX

--- cbyte/src/main/java/com/cbyte/PrimaryController.java
+++ cbyte/src/main/java/com/cbyte/PrimaryController.java
@@ -1,32 +1,33 @@
 package com.cbyte;
 
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
-import javafx.util.Callback;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import com.kodedu.terminalfx.TerminalBuilder;
+import com.kodedu.terminalfx.TerminalTab;
+import com.kodedu.terminalfx.config.TerminalConfig;
 
 import javafx.event.ActionEvent;
+
+
 
 // TODO: terminal, logic
 
@@ -40,20 +41,38 @@ public class PrimaryController implements Initializable {
     
     @FXML
     private TilePane tilePane;
+
+    @FXML
+    private TabPane tabPane;
     
     private static final double ICON_SIZE = 48.0; // Larger icon size
     private static final double TILE_SIZE = 100.0; // Size of each tile
 
+    // Used for the Tile pane
     private Image folderIcon = new Image(getClass().getResourceAsStream("/images/folder.png"));
     private Image fileIcon = new Image(getClass().getResourceAsStream("/images/cpp.png"));
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        tilePane = new TilePane();
+
+        //Configuration of Terminal
+        TerminalConfig darkConfig = new TerminalConfig();
+        darkConfig.setBackgroundColor(Color.rgb(16, 16, 16));
+        darkConfig.setForegroundColor(Color.rgb(240, 240, 240));
+        darkConfig.setCursorColor(Color.rgb(255, 255, 255, 0.5));
+
+        //Builds an instance of  Terminal
+        TerminalBuilder terminalBuilder = new TerminalBuilder(darkConfig);
+        TerminalTab terminalTab = terminalBuilder.newTerminal();
+
         tilePane.setPrefColumns(5); // Adjust the number of columns as needed
         tilePane.setHgap(10);
         tilePane.setVgap(10);
         scrollPane.setContent(tilePane);
+
+        //Adds terminalTab to tabPane
+        tabPane.getTabs().add(terminalTab);
+
     }
 
     @FXML
